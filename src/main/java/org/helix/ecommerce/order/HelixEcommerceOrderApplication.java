a0367cf9d9cbec53ef2c73d9420e35de183c189b
zhe.d.wang <zhe.d.wang@accenture.com>
2021-09-25 10:02:30 +0800

    Add Spring Boot, Spring Actutor, and Spring MVC,

---
+++ src/main/java/org/helix/ecommerce/order/HelixEcommerceOrderApplication.java
@@ -0,0 +1,13 @@
+package org.helix.ecommerce.order;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixEcommerceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelixEcommerceOrderApplication.class,args);
+    }
+}
